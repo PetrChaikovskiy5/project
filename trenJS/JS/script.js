acee9c235999701c1cfe2bcabdf2bd5f6943074b
PetrChaikovskiy5 <121093660+PetrChaikovskiy5@users.noreply.github.com>
2023-02-04 20:33:54 +0300

    Add files via upload

---
+++ trenJS/JS/script.js
@@ -0,0 +1,121 @@
+
+// let num = 'УЧИМ JS';
+// console.log(num);
+// function testBlock() {
+//   let myAge = 36;
+//   console.log(myAge);
+// }
+// testBlock ();
+// console.log(myAge);
+
+
+// let name = 'Deb';
+// let newName = 'Fer';
+// name = newName;
+// console.log(name);
+
+
+// let glass='blue';
+// let user;
+// let userName=58;
+// user=userName;
+// console.log(typeof user);
+
+
+// let users=5;
+// console.log (users);
+// users += 3+2;
+// console.log (users);
+// users *= 2;
+// console.log (users);
+
+
+// let addUser=2;
+// // let newUser= addUser++;
+// let newUser= ++addUser;
+// console.log (newUser);
+
+
+// console.log (0||1);
+// console.log (false||0||'ter'||'0');
+// let userName= '';
+// let nickName= 0;
+// let user= userName||nickName||'noName';
+// console.log (user);
+// let x=5;
+// let y=4;
+// x>y&&y++;
+// console.log (y);
+// console.log (4&&1);
+// console.log (true&&3&&'alse'&&45);
+// let userName= '';
+// let nickName= 0;
+// let user= userName&&nickName&&'noName';
+// console.log (user);
+// console.log (false&&3||true&&false);
+// let users=-1;
+// (users>0)&&console.log(`use ${users}`)|| console.log(++users);
+// console.log (!false && 58 || 18&&!'');
+// console.log ('35'>22);
+
+
+
+let message = 'Привет';
+if (2>5) {
+  console.log (message)
+};
+let x=14;
+let t=5;
+if (x>t){
+  console.log (message);
+};
+if ("1"){
+  console.log (message);
+};
+let sl=1.4;
+if (sl>5){
+  console.log ("Верно");
+}
+// else {
+//   console.log("Попробуй снова");
+// };
+// let number=1;
+// if (number>50){
+//   console.log ("Верно");
+// }
+// else if (number>30){
+//   console.log(`${number} больше 30`);
+// }
+// else if (number>20){
+//   console.log(`${number} больше 20`);
+// }
+// else if (number>2){
+//   console.log(`${number} больше 2`);
+// }
+// else {
+//   console.log("Попробуй снова");
+// };
+// let messageEnd;
+// if (1 > 2) {
+//   messageEnd = ", Вася!";
+// }
+// else {
+//   messageEnd = ", Оля!";
+// }
+// message += messageEnd;
+// console.log(message);
+// let messageEnd = (5 > 2) ? ", Вася!" : ", Оля!";
+// message += messageEnd;
+// console.log(message);
+// if (0){
+//   console.log ("logy");
+// } else if (" "){
+//   console.log ("Istina")
+// }
+
+
+let num=0;
+while (num<5) {
+  console.log(num);
+  num++;
+};
